4e50bffe3565d41df4be392f2912bcaf8d22e16e
a1792581384 <a1792581384@icloud.com>
2025-10-24 00:02:07 +0800

    Update workflow_download.py

--- usage/workflow_download.py
+++ usage/workflow_download.py
@@ -3,37 +3,36 @@ from从jmcomic导入*从……起jmcomic进口
 
 # 下方填入你要下载的本子的id，一行一个，每行的首尾可以有空白字符
 JM_albums="''''
-
-350324
+350234
 
 '''
 
 # 单独下载章节
-JM_photos=""''
+JM_photos="""
 
 
 
 '''
 
 
-defDef env(名称，默认值，trim=('[]'，'""'，""")：env(名称，默认值，trim=('[]'，'""'，"""))：
+defDefenv(名称，默认值，修剪=('[]'，'""'，""")：env(名称，默认值，trim=('[]'，'""'，"""))：
 导入操作系统导入操作系统
 value=os.getenv(名称，无)getenv(name，None)
-如果值为None或value=="：如果value为None或value="：
+如果值为None或价值=="：如果value为None或价值="：
 返回默认值返回默认值
 
 对于成对修剪：对于成对修剪：
-如果value.startswith(对[0])和value.endswith(对[1])：if value.startswith(对[0])和value.endswith(对[1])：
+如果value.startswith(对[0])和value.endswith(对[1])：if价值。startswith(对[0])和value.endswith(对[1])：
 value=value[1：-1][1：-1]
 
 返回值返回值
 
 
-def定义get_id_set(环境名称，给定)：get_id_set(env_name，givened)：
+def定义get_id_set(环境名称，给定)：get_id_set(环境名称，给定)：
 aid_set=set()set()
 对于[中的文本对于[中的文本
 鉴于，
-(环境(环境名称，")).replace('-'，'\n')，(环境(环境名称，")).replace('-'，'\n')，
+(环境(环境名称，")).替换('-'，'\n')，(环境(环境名称，")).替换('-'，'\n')，
     ]:
         aid_set.update(str_to_set(text))
 
